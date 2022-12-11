c91311d03f4d8be8473cf3ea9b87241806ac489f
Alex <def2maxim@yahoo.com>
2022-12-11 14:58:52 +0300

    feat(core): static

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from sqladmin import Admin
 from db.database import engine
 from _core.auth_admin import authentication_backend
-from repositories.admins.user_admin import UserAdmin, PostAdmin, PostCategoryAdmin
+from repositories.admins.user_admin import UserAdmin
+from repositories.admins.post_admin import PostAdmin, PostCategoryAdmin
 from _core import authentication
-from routers import user
+from routers import user, post
 
 app = FastAPI(title="Persefona API")
 
 app.include_router(user.router)
 app.include_router(authentication.router)
+app.include_router(post.router)
 
 # models.Base.metadata.create_all(engine)
 
@@ -21,3 +24,5 @@ admin = Admin(app, engine, base_url='/admin', authentication_backend=authenticat
 admin.add_view(UserAdmin)
 admin.add_view(PostAdmin)
 admin.add_view(PostCategoryAdmin)
+
+app.mount("/static", StaticFiles(directory='static'), name='static')
